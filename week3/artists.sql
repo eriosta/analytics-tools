b76a3cd088c41ac1a2086dfe934e54fd04ebb121
eriosta <eri@sienaspace.com>
2023-05-02 09:19:01 -0500

    Done with Week 3

--- week3/artists.sql
+++ week3/artists.sql
@@ -47,3 +47,70 @@ FROM invoices
 JOIN invoice_items
 ON 1=1
 ) as result;
+
+SELECT Name
+FROM tracks
+WHERE AlbumId = (
+  SELECT AlbumId
+  FROM albums
+  WHERE Title = 'Californication'
+);
+
+SELECT Name
+FROM (
+  SELECT Name,
+         ROW_NUMBER() OVER (ORDER BY TrackId) AS RowNumber
+  FROM tracks
+  WHERE AlbumId = (
+    SELECT AlbumId
+    FROM albums
+    WHERE Title = 'Californication'
+  )
+)
+WHERE RowNumber = 8;
+
+---12
+SELECT
+    Customer.FirstName || ' ' || Customer.LastName AS "Full Name",
+    Customer.Email,
+    COUNT(Invoice.InvoiceId) AS "Total Invoices",
+    Customer.City
+FROM customers Customer
+INNER JOIN invoices Invoice ON Customer.CustomerId = Invoice.CustomerId
+GROUP BY Customer.CustomerId
+HAVING "Full Name" = 'František Wichterlová'
+
+---13 
+SELECT tracks.Name AS track_name, albums.Title AS album_title, albums.ArtistId, tracks.TrackId 
+FROM tracks 
+JOIN albums ON tracks.AlbumId = albums.AlbumId;
+
+SELECT tracks.Name AS track_name 
+FROM tracks 
+JOIN albums ON tracks.AlbumId = albums.AlbumId 
+WHERE tracks.TrackId = 12 AND albums.Title = 'For Those About To Rock We Salute You';
+
+---14
+SELECT e1.LastName AS manager_last_name, e2.LastName AS employee_last_name
+FROM employees e1
+JOIN employees e2 ON e1.EmployeeId = e2.ReportsTo;
+
+---15
+SELECT artists.Name, artists.ArtistId
+FROM artists
+LEFT JOIN albums ON artists.ArtistId = albums.ArtistId
+WHERE albums.ArtistId IS NULL;
+
+---16
+SELECT FirstName, LastName
+FROM employees
+UNION
+SELECT FirstName, LastName
+FROM customers
+ORDER BY LastName DESC;
+
+---17
+SELECT COUNT(*) AS num_customers_different_cities
+FROM customers c
+JOIN invoices i ON c.CustomerId = i.CustomerId
+WHERE c.City <> i.BillingCity;
